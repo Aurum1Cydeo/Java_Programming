c080222ae8ffeb37080563e4c1f30ad4951eeb73
nataliiachobu <mebel2008@yahoo.com>
2022-07-05 09:30:29 -0700

    Abstract class and methods

---
+++ src/day34_abstractcion/car/CarTestObjects.java
@@ -0,0 +1,30 @@
+package day34_abstractcion.car;
+
+public class CarTestObjects {
+    public static void main(String[] args) {
+
+        Honda honda =new Honda("Accord","Black",2019,25000);
+        Audi audi=new Audi("Q6","Silver",2020,36000);
+        Tesla tesla=new Tesla("Model Y","Blue",2022,60000);
+        System.out.println(honda);
+        System.out.println(audi);
+        System.out.println(tesla);
+
+        System.out.println("----------------------------");
+        honda.stop();
+        audi.stop();
+        tesla.stop();
+
+        System.out.println("----------------------------");
+
+        honda.start();
+        audi.start();
+        tesla.start();
+
+        System.out.println("----------------------------");
+
+       audi.autoPark();
+       tesla.autoPilot();
+
+    }
+}
